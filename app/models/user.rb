11c7a59ea4d2f4129e1b611f48227392d8e61dab
hager-nasser22 <hager.nasser.32002@gmail.com>
2025-06-01 16:57:41 +0000

    Hager added

---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ApplicationRecord
+    has_many :created_posts, class_name: "Post", foreign_key: "user_id"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+users = User.create([
+  { name: "Abdo", email: "hager@gmail.com" },
+  { name: "Mona", email: "salwa@gmail.com" }
+])
+
+posts = Post.create([
+  { title: "Post 1", body: "Hello World", user_id: users[0].id },
+  { title: "Post 2", body: "Second one", user_id: users[1].id }
+])
+
+editors = Editor.create([
+  { name: "Editor A" },
+  { name: "Editor B" }
+])
+
+posts[0].editors << editors[0]
+posts[1].editors << [editors[0], editors[1]]
